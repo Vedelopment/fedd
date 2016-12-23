95fe02ca1f397edbd11c6cc7f50509773f114575
c00z <zcusimano@gmail.com>
2016-12-23 08:31:53 -0800

    Heroku test 2

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/fedd-api");
+mongoose.connect( process.env.MONGODB_URI || "YOUR CURRENT LOCALHOST DB CONNECTION STRING HERE" );
 
 module.exports.Restaurant = require("./restaurant.js");
